6ea69c8588d568545b49ebf56e48d5ab9c45f0b4
Alsu Stepanova <shaykhattarova.alsu@gmail.com>
2020-02-29 17:50:10 +0300

    scrolling to the right

--- app/src/main/java/com/example/looplayoutmanager/LoopLayoutManager.kt
+++ app/src/main/java/com/example/looplayoutmanager/LoopLayoutManager.kt
@@ -1,5 +1,6 @@
 package com.example.looplayoutmanager
 
+import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,31 +15,70 @@ class LoopLayoutManager(
 
     override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
         super.onLayoutChildren(recycler, state)
-        var accumulatedWidth = 0
-        var accumulatedHeight = 0
-        var counter = 0
-        while (accumulatedWidth < width) {
-            val view = recycler.getViewForPosition(counter % state.itemCount)
-            addView(view)
-            measureChildWithMargins(view, 0, 0)
-            layoutDecoratedWithMargins(
-                view,
-                accumulatedWidth,
-                accumulatedHeight,
-                accumulatedWidth + view.width,
-                accumulatedHeight + view.height
-            )
-            if (counter % spanCount < spanCount - 1) {
-                accumulatedHeight += view.height
-            } else {
-                accumulatedHeight = 0
-                accumulatedWidth += view.width
-            }
-            counter++
-        }
+        detachAndScrapAttachedViews(recycler)
+        fillToRight(0, 0, 0, recycler, state)
     }
 
     override fun canScrollHorizontally(): Boolean = true
 
     override fun canScrollVertically(): Boolean = false
+
+    override fun scrollHorizontallyBy(
+        dx: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State
+    ): Int {
+        Log.d("tag", "$dx")
+        offsetChildrenHorizontal(-dx)
+        if (dx > 0) {
+            // Значит элементы уходят налево
+            getChildAt(childCount - 1)
+                ?.takeIf {
+                    it.right < width
+                }
+                ?.let { view ->
+                    val fromPosition = getPosition(view).let { position ->
+                        (position + 1) % itemCount
+                    }
+                    fillToRight(view.right, 0, fromPosition, recycler, state)
+                }
+        } else if (dx < 0) {
+            // Значит элементы уходят направо
+        }
+        return dx
+    }
+
+    private fun fillToRight(
+        currentWidth: Int,
+        currentHeight: Int,
+        startFromPosition: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State
+    ) {
+        var accumulatedWidth = currentWidth
+        var accumulatedHeight = currentHeight
+        var counter = startFromPosition
+        var initialPosition = startFromPosition
+        while (accumulatedWidth < width) {
+            val view = recycler.getViewForPosition(counter % state.itemCount)
+            addView(view)
+            measureChildWithMargins(view, 0, 0)
+            val width = getDecoratedMeasuredWidth(view)
+            val height = getDecoratedMeasuredHeight(view)
+            layoutDecoratedWithMargins(
+                view,
+                accumulatedWidth,
+                accumulatedHeight,
+                accumulatedWidth + width,
+                accumulatedHeight + height
+            )
+            if ((counter - initialPosition) % spanCount == spanCount - 1) {
+                accumulatedHeight = 0
+                accumulatedWidth += view.width
+            } else {
+                accumulatedHeight += view.height
+            }
+            counter++
+        }
+    }
 }
--- app/src/main/java/com/example/looplayoutmanager/MainActivity.kt
+++ app/src/main/java/com/example/looplayoutmanager/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.looplayoutmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private val list = mutableListOf<Int>().apply {
-        for (i in 0 until 20) {
+        for (i in 0 until 7) {
             this.add(i)
         }
     }
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         with(findViewById<RecyclerView>(R.id.recycler_view)) {
-            layoutManager = LoopLayoutManager(2)
+            layoutManager = LoopLayoutManager(3)
             adapter = LoopAdapter().apply { itemList = list }
         }
     }
